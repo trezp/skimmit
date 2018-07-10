40d838a175e2d56f80c3b45b741d73a4ce2cc875
Treasure <treasure.rose@gmail.com>
2018-07-10 09:35:50 -0700

    phase 1 with instructions

--- app.js
+++ app.js
@@ -3,23 +3,30 @@ const phaseOne = new Vue({
   data: {
     post: {
       title: 'Really Important News', 
-      post: 'This is a summary of what the post is about.',
+      summary: 'This is a summary of what the post is about.',
       thumbnail: 'https://placeimg.com/75/75/any',
-      votes: 6,
+      alt: 'Thumbnail of image',
+      votes: 0,
     },
   },
   methods: {
     increment(){
-      this.post.votes = this.post.votes + 1;
+      this.post.votes += 1;
     },
     decrement(){
-      this.post.votes = this.post.votes - 1;
-    }
-  },
-  computed: {
-    submitted: function(){
-      const date = new Date(); 
-      return date;
+      this.post.votes -= 1;
     }
   }
 });
+
+//PHASE ONE
+// 1. Create a new Vue instance with the necessary options
+// 2. Create a data property called "post". "post" should be an object with the following
+// properties: title, summary, thumbnail and votes
+// 3. Make up data for each property. The value of "votes" should be zero
+// 4. Display the post's vote count, image thumbnail, alt text, title, and summary in the Vue template
+// 5. Create a method called "increment" that adds 1 vote to "votes"
+// 6. Create a method called "decrement" that substracts 1 vote from "votes"
+// 7. Add the methods to the span elements with the div with a id of "voteCounter"
+
+//PHASE TWO
